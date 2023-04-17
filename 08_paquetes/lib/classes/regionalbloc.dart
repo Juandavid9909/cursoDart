221d849a0c70f41603b3791e4725ad2d78a198c3
Juan David <juandavid918@gmail.com>
2023-04-17 14:10:33 -0500

    Peticiones http realizadas y curso finalizado

---
+++ 08_paquetes/lib/classes/regionalbloc.dart
@@ -0,0 +1,31 @@
+class RegionalBloc {
+  RegionalBloc({
+    required this.acronym,
+    required this.name,
+    required this.otherNames,
+    this.otherAcronyms,
+  });
+
+  String acronym;
+  String name;
+  List<String> otherNames;
+  List<String>? otherAcronyms;
+
+  factory RegionalBloc.fromJson(Map<String, dynamic> json) => RegionalBloc(
+        acronym: json["acronym"],
+        name: json["name"],
+        otherNames: List<String>.from(json["otherNames"].map((x) => x)),
+        otherAcronyms: json["otherAcronyms"] == null
+            ? []
+            : List<String>.from(json["otherAcronyms"]!.map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "acronym": acronym,
+        "name": name,
+        "otherNames": List<dynamic>.from(otherNames.map((x) => x)),
+        "otherAcronyms": otherAcronyms == null
+            ? []
+            : List<dynamic>.from(otherAcronyms!.map((x) => x)),
+      };
+}
